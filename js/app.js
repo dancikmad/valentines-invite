9e4aaa572bba104d62346618cbcbc275290c15e1
Daniel Crigan <crgdaniel@yahoo.com>
2025-02-04 19:45:28 +0000

    fix: yes button

--- js/app.js
+++ js/app.js
@@ -23,29 +23,123 @@ document.querySelectorAll('button').forEach(function (button) {
 
             particlesJS('particles-js', {
                 "particles": {
-                    "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
-                    "color": { "value": "#FF0000" },
+                    "number": {
+                    "value": 80,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                    },
+                    "color": {
+                    "value": "#FF0000"
+                    },
                     "shape": {
-                        "type": "image",
-                        "image": { "src": "/assets/heart.png", "width": 50, "height": 50 }
+                    "type": "image",
+                    "stroke": {
+                        "width": 0,
+                        "color": "#FF0000"
                     },
-                    "opacity": { "value": 0.5 },
-                    "size": { "value": 50, "random": true, "anim": { "enable": true, "speed": 4.8 } },
-                    "move": { "enable": true, "speed": 3, "direction": "bottom", "random": true, "out_mode": "out" }
-                },
-                "interactivity": {
-                    "events": {
-                        "onhover": { "enable": true, "mode": "repulse" },
-                        "onclick": { "enable": true, "mode": "bubble" },
-                        "resize": true
+                    "polygon": {
+                        "nb_sides": 3
                     },
-                    "modes": {
-                        "bubble": { "distance": 0, "size": 40, "duration": 0.6, "opacity": 1, "speed": 3 },
-                        "repulse": { "distance": 300, "duration": 0.4 }
+                    "image": {
+                        "src": "../assets/heart.png",
+                        "width": 50,
+                        "height": 50
+                    }
+                    },
+                    "opacity": {
+                    "value": 0.5,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 1,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                    },
+                    "size": {
+                    "value": 50,
+                    "random": true,
+                    "anim": {
+                        "enable": true,
+                        "speed": 4.795204795204795,
+                        "size_min": 0,
+                        "sync": false
+                    }
+                    },
+                    "line_linked": {
+                    "enable": true,
+                    "distance": 32.06824121731046,
+                    "color": "#ffffff",
+                    "opacity": 0.03206824121731046,
+                    "width": 0
+                    },
+                    "move": {
+                    "enable": true,
+                    "speed": 3,
+                    "direction": "bottom",
+                    "random": true,
+                    "straight": false,
+                    "out_mode": "out",
+                    "bounce": false,
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
                     }
                 },
-                "retina_detect": true
+                "interactivity": {
+                    "detect_on": "canvas",
+                    "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "repulse"
+                    },
+                    "onclick": {
+                        "enable": true,
+                        "mode": "bubble"
+                    },
+                    "resize": true
+                    },
+                    "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                        "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 0,
+                        "size": 40,
+                        "duration": 0.6496617698410762,
+                        "opacity": 1,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 300,
+                        "duration": 0.4
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                    }
+                },
+                "retina_detect": true,
+                "config_demo": {
+                "hide_card": false,
+                "background_color": "#b61924",
+                "background_image": "",
+                "background_position": "50% 50%",
+                "background_repeat": "no-repeat",
+                "background_size": "cover"
+                }
             });
+
         }
 
         if(e.target.id === "no") {
