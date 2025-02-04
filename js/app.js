d5b320d447c8ac3c489887b7bc59363cc68ecfa6
Daniel Crigan <crgdaniel@yahoo.com>
2025-02-04 19:54:53 +0000

    fixed 2

--- js/app.js
+++ js/app.js
@@ -43,7 +43,7 @@ document.querySelectorAll('button').forEach(function (button) {
                         "nb_sides": 3
                     },
                     "image": {
-                        "src": "assets/Heart.png",
+                        "src": "/assets/heart.png",
                         "width": 50,
                         "height": 50
                     }
